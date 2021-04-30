b349bb6a17cb62607f1baf5c8afcfaf1f8094e1b
Omisha Patel <64633546+OmishaPatel@users.noreply.github.com>
2021-04-30 13:52:39 -0500

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,33 +1,58 @@
-import javax.swing.JOptionPane;
 public class Quiz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		boolean is_true = false;
-		//While loop will execute until is_true returns true 
-		while (!is_true) {
-		String question = "What country won the very first FIFA World Cup in 1930?\n";
-		question += "A. Brazil\n";
-		question += "B. Colombia\n";
-		question += "C. Uruguay\n";
-		question += "D. Peru\n";
-		question += "E. Bolivia\n";
-		String answer = JOptionPane.showInputDialog(question);
-		answer = answer.toUpperCase();
-		//While loop will execute until is_true returns true 
-		if (answer.equals("C")) {
-			 JOptionPane.showMessageDialog(null,"Correct!");
-			 is_true = true;
-			 break;
-			}
-		else if (answer.equals("A") || answer.equals("B") || answer.equals("D")|| answer.equals("E") ) {
-			 JOptionPane.showMessageDialog(null,"Incorrect. Please try again.");
-		}
-		else {
-			JOptionPane.showMessageDialog(null,"Invalid answer. Please enter A, B, C, D, or E.");
-		}
-	}
+		MultipleChoiceQuestion question = new MultipleChoiceQuestion(
+				"What is the name of German Airline?",
+				 "Delta",
+				 "United",
+				 "Luftansa",
+				 "Kingfisher",
+				 "Avianca",
+				 "c");
+		question.check();
+		 //resetting the reference value of question variable by creating new object
+		question = new MultipleChoiceQuestion(
+				"What is the capital city of Chile?",
+				 "Santiago",
+				 "Santa Marta",
+				 "San Andres",
+				 "Buenos Aires",
+				 "Bogota",
+				 "a");
+		question.check();
 		
-	}
-
+		question = new MultipleChoiceQuestion(
+				"What country has the most islands in the world?",
+				 "Norway",
+				 "Sweden",
+				 "USA",
+				 "Poland",
+				 "Spain",
+				 "b");
+		question.check();
+		
+		question = new MultipleChoiceQuestion(
+				"How many teeth does a adult human have?",
+				 "30",
+				 "34",
+				 "28",
+				 "32",
+				 "31",
+				 "d");
+		question.check();
+		
+		question = new MultipleChoiceQuestion(
+				"Which is the biggest state in USA?",
+				 "Texas",
+				 "New York",
+				 "California",
+				 "Arizona",
+				 "Alaska",
+				 "e");
+		question.check();
+		
+		MultipleChoiceQuestion.showResults();
+		
+			}
+	
 }
