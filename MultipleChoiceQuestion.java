80a1c152a0c050937e2104311a43349ba1169a1c
Omisha Patel <64633546+OmishaPatel@users.noreply.github.com>
2021-05-06 23:50:22 -0400

    Update MultipleChoiceQuestion.java

--- MultipleChoiceQuestion.java
+++ MultipleChoiceQuestion.java
@@ -1,11 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class MultipleChoiceQuestion {
-	static int nQuestions = 0;
-	static int nCorrect = 0;
-	
-	String question; // instance variable
-	String correctAnswer;// instance variable
+public class MultipleChoiceQuestion extends Question {
 	
 	MultipleChoiceQuestion(String query, String a, String b, String
 			c, String d, String e, String answer) {
@@ -35,26 +30,9 @@ public class MultipleChoiceQuestion {
 		}
 		}
 	}
-		 void check() {
-			 nQuestions++;
-			 String answer = ask();
-			 if (answer.equals(correctAnswer)) {
-				 JOptionPane.showMessageDialog(null,"Correct!");
-				 nCorrect++;
-				}
-			 else {
-				 JOptionPane.showMessageDialog(null,"Incorrect. The Correct Answer is " + correctAnswer);
-			}
-		}
-		 static void showResults() {
-			 JOptionPane.showMessageDialog(null,nCorrect+" correct out of "+nQuestions+" questions");
-		 }
+		 
 		
 }
-	
-	
-	
-	
 	
 	
 
